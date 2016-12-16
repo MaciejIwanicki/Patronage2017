691d2254147eca276ac5943fef85057974202b22
Maciej <iwanickimaciek9944@gmail.com>
2016-12-17 00:25:19 +0100

    Removed folder from repo

--- Voue/index.js
+++
@@ -1,6 +0,0 @@
-// start up the server
-var server = require("http-server/bin/http-server");
-
-// load the page it is serving
-var got = require("got");
-(await got.get("http://localhost:8080")).body
